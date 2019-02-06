db18829177169bb24990d6a7f875a40629335091
Drew Williams <williams.r.drew@gmail.com>
2019-02-06 10:45:41 -0500

    changed to map type

--- FieldObserver/src/main/java/org/team2053/FieldObserver/App.java
+++ FieldObserver/src/main/java/org/team2053/FieldObserver/App.java
@@ -18,14 +18,4 @@ public class App extends Plugin {
 		// TODO Auto-generated method stub
 		return ImmutableList.of(WidgetType.forAnnotatedWidget(FieldObserver.class));
 	}
-
-	@Override
-	public List<DataType> getDataTypes() {
-		return ImmutableList.of(new RobotPoseData());
-	}
-
-	@Override
-	public Map<DataType, ComponentType> getDefaultComponents() {
-		return Map.of(RobotPoseData.Instance, WidgetType.forAnnotatedWidget(FieldObserver.class));
-	}
 }
--- FieldObserver/src/main/java/org/team2053/FieldObserver/FieldObserver.java
+++ FieldObserver/src/main/java/org/team2053/FieldObserver/FieldObserver.java
@@ -1,19 +1,21 @@
 package org.team2053.FieldObserver;
 
+import edu.wpi.first.shuffleboard.api.data.MapData;
 import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
-@Description(dataTypes = { RobotPoseData.class }, name = "Field Observer Widget" )
+@Description(dataTypes = { MapData.class }, name = "Field Observer Widget" )
 @ParametrizedController(value = "FieldObserver.fxml")
-public class FieldObserver extends SimpleAnnotatedWidget<RobotPose> {
+public class FieldObserver extends SimpleAnnotatedWidget<MapData> {
 
 	@FXML
 	protected Pane _thePane;
@@ -22,17 +24,44 @@ public class FieldObserver extends SimpleAnnotatedWidget<RobotPose> {
 	
 	private ObjectProperty<Image> imageToBeDrawn = new SimpleObjectProperty<Image>(new Image(getClass().getResource("2019-field.jpg").toExternalForm()));
 	
+	private SimpleStringProperty _xPositionKey = new SimpleStringProperty(this, "xImageKey", "");
+	private SimpleStringProperty _yPositionKey = new SimpleStringProperty(this, "yImageKey", "");
+	private SimpleStringProperty _headingAngleKey = new SimpleStringProperty(this, "headingImageKey", "");
+	
+	public String getXImageKey() {
+		return _xPositionKey.getValue();
+	}
+	
+	public void setXImageKey(String x) {
+		_xPositionKey.setValue(x);
+	}
+	
+	public String getYImageKey() {
+		return _yPositionKey.getValue();
+	}
+	
+	public void setYImageKey(String y) {
+		_yPositionKey.setValue(y);
+	}
+	
+	public String getHeadingImageKey() {
+		return _headingAngleKey.getValue();
+	}
+	
+	public void setHeadingImageKey(String h) {
+		_headingAngleKey.setValue(h);
+	}
+	
 	public Pane getView() {
 		
 		GraphicsContext gc = _theCanvas.getGraphicsContext2D();
-		gc.drawImage(imageToBeDrawn.get(), 0, 0);
-		gc.fillOval(this.getData().getXPos(), this.getData().getYPos(), 10, 10);
-		
+		gc.drawImage(imageToBeDrawn.get(), 0, 0);		
 		return _thePane;
 	}
 
 	@FXML
 	public void initialize() {
+		//this.exportProperties(_xPositionKey, _yPositionKey, _headingAngleKey);
 		GraphicsContext gc = _theCanvas.getGraphicsContext2D();
 		gc.drawImage(imageToBeDrawn.get(), 0, 0);
 	}
